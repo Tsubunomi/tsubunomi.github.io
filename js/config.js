8f3a2cc5e9e9dbe0cddd3fbcc863ecd551755fb9
Tsubunomi <Tsubunomi@outlook.com>
2026-06-22 21:01:26 +0900

    Update config.js

--- js/config.js
+++ js/config.js
@@ -13,7 +13,7 @@ const CONFIG = {
     // Apps Script API
 
     API_URL:
-    "https://script.google.com/macros/s/AKfycbyY7V-NnPRW5dh_1wnR-__7_UB6_jXzATA1Cb1zeK4UTVlVK7yLBZ--zAz6axz0huKVMQ/exec",
+    "https://script.google.com/macros/s/AKfycbwuocqG4hOFQuHKdQ75aaU2vZD9CTr30mXWQN7zRssA1EXbQ_N5XmU4OAl4lsxllQ5O/exec",
 
     // 最近収穫したもの
 
