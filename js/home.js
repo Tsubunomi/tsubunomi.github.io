6f5c61555461812c63de275b6874086d31b408f9
Tsubunomi <Tsubunomi@outlook.com>
2026-06-24 21:42:30 +0900

    Remove loadToday function from home.js
    Removed the loadToday function and its related code.

--- js/home.js
+++ js/home.js
@@ -1,27 +1,4 @@
-loadToday();
 loadRecent();
-
-async function loadToday(){
-
-    showLoading(
-        "today",
-        "今日の実りを収穫しています..."
-    );
-
-    const data =
-    await api.getToday();
-
-    if(!data){
-        showError("today");
-        return;
-    }
-
-    document.getElementById(
-        "today"
-    ).innerHTML =
-    createToday(data);
-
-}
 
 async function loadRecent(){
 
